d2b50f86d16ccf7b7e6c847f5626d158826684c9
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-03-30 01:51:02 +0300

    Partly implemented normalization.

--- engine/implementation/expressions/expression_normalizer.cpp
+++ engine/implementation/expressions/expression_normalizer.cpp
@@ -10,39 +10,94 @@ namespace dm
 namespace
 {
 
+/////////// ExpressionNormalizerVisitor /////////////
+
 class ExpressionNormalizerVisitor : public ExpressionVisitor
 {
 public:
    ExpressionNormalizerVisitor();
 
+   OperationType GetOperation() const;
+   bool GetIsLeaf() const;
+
    // ExpressionVisitor
    virtual void Visit(LiteralExpression& expression) override;
    virtual void Visit(ParamRefExpression& expression) override;
    virtual void Visit(OperationExpression& expression) override;
+
+private:
+   // Operation which in each operation expression node is the subtree.
+   OperationType m_operation;
+   // Is current expression is a leaf of the subtree.
+   bool m_is_leaf;
 };
 
-ExpressionNormalizerVisitor::ExpressionNormalizerVisitor()
+ExpressionNormalizerVisitor::ExpressionNormalizerVisitor() :
+   m_operation(OperationType::None), m_is_leaf(false)
 {
 }
 
+OperationType ExpressionNormalizerVisitor::GetOperation() const
+{
+   return m_operation;
+}
+
+bool ExpressionNormalizerVisitor::GetIsLeaf() const
+{
+   return m_is_leaf;
+}
+
+
 void ExpressionNormalizerVisitor::Visit(LiteralExpression& expression)
 {
+   m_is_leaf = true;
 }
 
 void ExpressionNormalizerVisitor::Visit(ParamRefExpression&)
 {
+   m_is_leaf = true;
 }
 
 void ExpressionNormalizerVisitor::Visit(OperationExpression& expression)
 {
+   const long child_count = expression.GetChildCount();
+   const bool is_associative = IsOperationAssociative(expression.GetOperation());
+
+   for (long index = 0; index < child_count; ++index)
+   {
+      ExpressionNormalizerVisitor child_visitor;
+      expression.GetChild(index)->Accept(child_visitor);
+
+      // An expression can be marked as potentionally normalizable if:
+      //    - All children are leaves or operations
+      //      with the same operation type, as current expression.
+      // If the current operation is not associative additional condition should be checked:
+      //    - Only the first child can be not leaf.
+
+      // Here is negated and simplified condtion:
+
+      if (
+            !child_visitor.GetIsLeaf() && 
+            (
+               (!is_associative && index != 0) || 
+               child_visitor.GetOperation() == expression.GetOperation()
+            )
+         )
+      {
+         return;
+      }
+   }
+
+   m_operation = expression.GetOperation();
 }
+
+/////////// ExpressionLiniarizerVisitor /////////////
 
 }; // namespace
 
 void NormalizeExpression(TExpressionPtr& expression)
 {
    assert(expression.get() != nullptr);
-
    ExpressionNormalizerVisitor visitor;
    expression->Accept(visitor);
 }
