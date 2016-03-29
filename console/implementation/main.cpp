6a3d4b3b0561909878e51e304de2ee147edf0366
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-03-29 23:00:04 +0300

    Support of file name specification in console parameters.

--- console/implementation/main.cpp
+++ console/implementation/main.cpp
@@ -3,20 +3,24 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
-int main()
+int ProcessStream(std::istream& stream)
 {
    dm::TIEnginePtr engine = dm::CreateEngine();
 
-   std::cerr << "Enter commands to interact with the engine. Enter 'exit' to quit the program." << std::endl;
-
    std::string str;
-   while (true)
+   while (!stream.eof())
    {
-      std::getline(std::cin, str);
+      std::getline(stream, str);
       if (str == "exit")
       {
          break;
+      }
+
+      if (str.empty())
+      {
+         continue;
       }
 
       try
@@ -29,7 +33,33 @@ int main()
          std::cout << "Error: " << ex.GetDescription() << std::endl;
       }
    }
-
    return 0;
 }
 
+int main(int argc, char* argv[])
+{
+   if (argc > 2)
+   {
+      std::cerr << "Wrong number of parameters." << std::endl;
+      return 1;
+   }
+
+   if (1 == argc)
+   {
+      std::cout << "DM Console utility. Copyright (c) 2016 Roman Lapitsky." << std::endl 
+                << std::endl
+                << "Enter commands to interact with the engine. Enter 'exit' to quit the program." << std::endl
+                << std::endl;
+      return ProcessStream(std::cin);
+   }
+
+   // Process commands from file
+   std::ifstream ifstr(argv[1]);
+   if (!ifstr.is_open())
+   {
+      std::cerr << "Cannot open file '" << argv[1] << "'." << std::endl;
+      return 2;
+   }
+
+   return ProcessStream(ifstr);
+}
--- test/run_tests.cmd
+++ test/run_tests.cmd
@@ -4,15 +4,12 @@ set COMPILER=mingw
 set CONFIGURATION=debug
 set TEST_COMMAND=..\build_%COMPILER%\%CONFIGURATION%\bin\console.exe
 
-rem TESTS LIST BEGIN
-   call :run_one_test function_test
-   call :run_one_test function_table
-rem TESTS LIST END
-
+call :run_one_test function_test
+call :run_one_test function_table
 goto :eof
 
 :run_one_test
-%TEST_COMMAND% < %1.in > %1.out 2> NUL
+%TEST_COMMAND% %1.in > %1.out 2> NUL
 echo n | comp %1.out %1.exp > NUL 2> NUL
 if ERRORLEVEL 1 (echo %1...Fail & exit 1) else (echo %1...OK & del %1.out)
 goto :eof
