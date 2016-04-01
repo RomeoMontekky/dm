5309f7dd2ecf69a1126c8a8253609bdd8b1bc90c
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-04-01 17:37:53 +0300

    Partly implemented partial simplification.

--- engine/implementation/expressions/expression_simplifier.cpp
+++ engine/implementation/expressions/expression_simplifier.cpp
@@ -4,6 +4,7 @@
 
 #include "../common/local_array.h"
 
+#include <algorithm>
 #include <cassert>
 
 namespace dm
@@ -55,46 +56,93 @@ void ExpressionSimplifierVisitor::Visit(LiteralExpression& expression)
 
 void ExpressionSimplifierVisitor::Visit(OperationExpression& expression)
 {
-   const long child_count = expression.GetChildCount();
+   long child_count = expression.GetChildCount();
 
-   LOCAL_ARRAY(LiteralType, children_values, child_count);
-   LOCAL_ARRAY(bool, children_is_raws, child_count);
+   LOCAL_ARRAY(LiteralType, child_values, child_count);
+   LOCAL_ARRAY(bool, child_is_raws, child_count);
 
-   bool is_all_actual_values = true;
+   long actual_values_count = 0;
+   long first_actual_values_count = 0;
+
    for (long index = 0; index < child_count; ++index)
    {
       ExpressionSimplifierVisitor child_visitor;
       expression.GetChild(index)->Accept(child_visitor);
 
-      children_values[index] = child_visitor.GetValue();
-      children_is_raws[index] = child_visitor.GetIsRaw();
+      child_values[index] = child_visitor.GetValue();
+      child_is_raws[index] = child_visitor.GetIsRaw();
 
-      if (LiteralType::None == child_visitor.GetValue())
+      if (child_values[index] != LiteralType::None)
       {
-         is_all_actual_values = false;
+         ++actual_values_count;
+         if (first_actual_values_count >= 0)
+         {
+            ++first_actual_values_count;
+         }
+      }
+      else
+      {
+         first_actual_values_count = -1;
       }
    }
 
-   // If all children expressions have actual values, then we can
+   const bool is_children_movable = 
+      IsOperationCommutative(expression.GetOperation()) && 
+      IsOperationAssociative(expression.GetOperation());
+
+   // If all child expressions have actual values, then we can
    // simplify the whole operation expression to a calculated value.
-   if (is_all_actual_values)
+   if (child_count == actual_values_cout)
    {
-      m_value = PerformOperation(expression.GetOperation(), children_values, child_count);
+      m_value = PerformOperation(expression.GetOperation(), child_values, child_count);
+      return;
    }
-   // TODO: Partial simplification
-   //else if ()
-   //{
-   //}
-   else
+   else if (is_children_movable && actual_values_cout > 1)
    {
-      // If not all child expressions have actual values, then we need to
-      // simplify at least those who have and is not yet simplified.
-      for (long index = 0; index < child_count; ++index)
+      // In this case we can perform operation in any order, so we must do following:
+      //    1. Collect all actual values in a single array;
+      //    2. Calculate the result of operation over this array;
+      //    3. Remove all expressions that correspond to actual values;
+      //    4. Add one literal expression which will hold the calculated result.
+
+      LOCAL_ARRAY(LiteralType, actual_values, actual_values_count);
+      std::copy_if(child_values, child_values + child_count, actual_values, 
+         [](LiteralType literal) { return literal != LiteralType::None });
+
+      long index = 0;
+      while (index < child_count)
       {
-         if (children_values[index] != LiteralType::None && !children_is_raws[index])
+         if (child_values[index] != LiteralType::None)
          {
-            expression.GetChild(index) = std::make_unique<LiteralExpression>(children_values[index]);
+            expression.RemoveChild(index);
+            std::copy(child_values + index + 1, child_values + child_count, child_values + index);
+            --child_count;
          }
+         else
+         {
+            ++index;
+         }
+      }
+
+      const LiteralType value = PerformOperation(expression.GetOperation(), actual_values, actual_values_count);
+      expression.InsertChild(child_count, std::make_unique<LiteralExpression>(value));
+
+      return;
+   }
+   else if (!is_children_movable && first_actual_values_count > 1)
+   {
+      // In this case we cann't move operards, so we can simplify only first actual values.
+
+      // TODO: Implement
+   }
+
+   // If not all child expressions have actual values, then we need to
+   // simplify at least those who have and is not yet simplified.
+   for (long index = 0; index < child_count; ++index)
+   {
+      if (child_values[index] != LiteralType::None && !child_is_raws[index])
+      {
+         expression.GetChild(index) = std::make_unique<LiteralExpression>(child_values[index]);
       }
    }
 
