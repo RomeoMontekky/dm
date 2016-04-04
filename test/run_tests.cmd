a7264dfca116a19921dedfe4b6bab7bf309645de
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-04-04 20:41:03 +0300

    Removed nulling of error stream to see problems of test running.

--- test/run_tests.cmd
+++ test/run_tests.cmd
@@ -13,7 +13,7 @@ call :run_one_test function_table
 goto :eof
 
 :run_one_test
-%TEST_COMMAND% %1.in > %1.out 2> NUL
+%TEST_COMMAND% %1.in > %1.out
 echo n | comp %1.out %1.exp > NUL 2> NUL
 if ERRORLEVEL 1 (echo %1...Fail) else (echo %1...OK & del %1.out)
 goto :eof
