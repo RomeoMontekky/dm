a024ffb483ca92517034c62619638d425d6b4183
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-04-05 17:10:04 +0300

    Implemented copy constructor for Variable.

--- engine/implementation/variables/variable.cpp
+++ engine/implementation/variables/variable.cpp
@@ -1,4 +1,5 @@
 #include "variable.h"
+#include "../expressions/expression_param_ref.h"
 
 #include <cassert>
 
@@ -18,7 +19,17 @@ Variable::Variable(const StringPtrLen& name) :
 Variable::Variable(const StringPtrLen& name, const Variable& rhs) :
    VariableDeclaration(rhs), m_expression()
 {
-   // TODO: Copy expression, changing param-refs to new variable
+   const long param_count = GetParameterCount();
+   
+   TExpressionPtrVector replace_params;
+   replace_params.reserve(param_count);
+   for (long index = 0; i < param_count; ++i)
+   {
+      replace_params.push_back(
+         std::unique_ptr<LiteralExpression>(GetParameter(index), index));
+   }
+
+   m_expression = rhs.m_expression->CloneWithSubstitution(replace_params);
 }
 
 void Variable::SetExpression(TExpressionPtr&& expression)
