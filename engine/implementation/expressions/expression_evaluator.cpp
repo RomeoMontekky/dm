07150d5bb362c874f817606e85775e3304177f29
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-04-15 15:12:15 +0300

    Expression comparator renamed to expression determier and moved into expression_evaluator.cpp.

--- engine/implementation/expressions/expression_evaluator.cpp
+++ engine/implementation/expressions/expression_evaluator.cpp
@@ -10,21 +10,90 @@ namespace dm
 namespace
 {
 
-class ExpressionEvaluatorVisitor : public ExpressionVisitor
+///////////// DetermierExpressionVisitor ////////////
+
+class DetermierExpressionVisitor : public ConstExpressionVisitor
 {
 public:
-   ExpressionEvaluatorVisitor();
+   DetermierExpressionVisitor();
+   
+   LiteralType GetLiteral() const;
+   long GetParamIndex() const;
+   OperationType GetOperation() const;
 
-   // ExpressionVisitor
-   virtual void Visit(OperationExpression& expression) override;
+   // ConstExpressionVisitor
+   virtual void Visit(const LiteralExpression& expression) override;
+   virtual void Visit(const ParamRefExpression& expression) override;
+   virtual void Visit(const OperationExpression& expression) override;
+
+   bool operator ==(const DetermierExpressionVisitor& rhs) const;
+
+private:
+   LiteralType m_literal;
+   long m_param_index;
+   OperationType m_operation;
+   std::vector<DetermierExpressionVisitor> m_child_comparators;
 };
 
-ExpressionEvaluatorVisitor::ExpressionEvaluatorVisitor()
+DetermierExpressionVisitor::DetermierExpressionVisitor() :
+    m_literal(LiteralType::None),
+    m_param_index(-1),
+    m_operation(OperationType::None),
+    m_child_comparators()
 {
 }
 
-void ExpressionEvaluatorVisitor::Visit(OperationExpression& expression)
+LiteralType DetermierExpressionVisitor::GetLiteral() const
 {
+   return m_literal;
+}
+
+long DetermierExpressionVisitor::GetParamIndex() const
+{
+   return m_param_index;
+}
+
+OperationType DetermierExpressionVisitor::GetOperation() const
+{
+   return m_operation;
+}
+
+void DetermierExpressionVisitor::Visit(const LiteralExpression& expression)
+{
+   m_literal = expression.GetLiteral();
+}
+
+void DetermierExpressionVisitor::Visit(const ParamRefExpression& expression)
+{
+   m_param_index = expression.GetParamIndex();
+}
+
+void DetermierExpressionVisitor::Visit(const OperationExpression& expression)
+{
+   m_operation = expression.GetOperation();
+   m_child_comparators.resize(expression.GetChildCount());
+
+   const long child_count = expression.GetChildCount();
+   for (long index = 0; index < child_count; ++index)
+   {
+      expression.GetChild(index)->Accept(m_child_comparators[index]);
+   }
+}
+
+bool DetermierExpressionVisitor::operator ==(const DetermierExpressionVisitor& rhs) const
+{
+   return
+   (
+      (m_literal == rhs.m_literal) &&
+      (m_param_index == rhs.m_param_index) &&
+      (m_operation == rhs.m_operation) &&
+      (m_child_comparators == rhs.m_child_comparators)
+   );
+}
+
+void EvaluateExpression(TExpressionPtr& expression, DetermierExpressionVisitor& determier)
+{
+   
 }
 
 } // namespace
@@ -32,8 +101,11 @@ void ExpressionEvaluatorVisitor::Visit(OperationExpression& expression)
 void EvaluateExpression(TExpressionPtr& expression)
 {
    assert(expression.get() != nullptr);
-   ExpressionEvaluatorVisitor visitor;
-   expression->Accept(visitor);
+   
+   DetermierExpressionVisitor determier;
+   expression->Accept(determier);
+   
+   EvaluateExpression(expression, determier);
 }
 
 } // namespace dm
