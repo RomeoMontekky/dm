52455f289226aaffd3cbf6ab69160cd9d6d9b339
ra.lapickiy <ra.lapickiy@tensor.ru>
2016-06-15 14:54:53 +0300

    Calculator re-factored to use expression utils.

--- engine/implementation/expressions/expression_calculator.cpp
+++ engine/implementation/expressions/expression_calculator.cpp
@@ -1,4 +1,5 @@
 #include "expression_calculator.h"
+#include "expression_utils.h"
 #include "expressions.h"
 
 #include "../common/local_array.h"
@@ -17,28 +18,28 @@ LiteralType CalculateExpression(const TExpressionPtr& expr, const LiteralType pa
    {
       case ExpressionType::Literal:
       {
-         value = static_cast<const LiteralExpression*>(expr.get())->GetLiteral();
+         value = CastToLiteral(expr).GetLiteral();
          break;
       }
 
       case ExpressionType::ParamRef:
       {
-         value = param_values[static_cast<const ParamRefExpression*>(expr.get())->GetParamIndex()];
+         value = param_values[CastToParamRef(expr).GetParamIndex()];
          break;
       }
 
       case ExpressionType::Operation:
       {
-         auto expression = static_cast<const OperationExpression*>(expr.get());
-         const long child_count = expression->GetChildCount();
+         auto& expression = CastToOperation(expr);
+         const auto child_count = expression.GetChildCount();
 
          LOCAL_ARRAY(LiteralType, child_values, child_count);
-         for (long index = child_count - 1; index >= 0; --index)
+         for (auto index = child_count - 1; index >= 0; --index)
          {
-            child_values[index] = CalculateExpression(expression->GetChild(index), param_values);
+            child_values[index] = CalculateExpression(expression.GetChild(index), param_values);
          }
 
-         value = PerformOperation(expression->GetOperation(), child_values, child_count);
+         value = PerformOperation(expression.GetOperation(), child_values, child_count);
 
          break;
       }
